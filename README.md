61bda05351b221d9d331c29ede3a774fc2740e24
Tom. A. Broughton <tom.a.broughton@gmail.com>
2017-12-20 18:42:27 +0000

    gulp and sass files prep for css and js gen

---
+++ README.md
@@ -0,0 +1,16 @@
+# Transformer theme for Hugo
+
+* A bootstrap 4 theme for hugo
+* Uses Sass and Gulp
+* [Originates from Start Bootstrap - Resume](https://startbootstrap.com/template-overviews/resume/)
+
+Run npm install
+
+## Gulp Tasks
+
+- `gulp` the default task that builds everything
+- `gulp dev` browserSync opens the project in your default browser and live reloads when changes are made
+- `gulp sass` compiles SCSS files into CSS
+- `gulp minify-css` minifies the compiled CSS file
+- `gulp minify-js` minifies the themes JS file
+- `gulp copy` copies dependencies from node_modules to the vendor directory
---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Transformer Theme Builder - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/tabroughton/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+  return gulp.src('scss/transformer.scss')
+    .pipe(sass())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('static/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('static/css/transformer.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('static/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Minify custom JS
+gulp.task('minify-js', function() {
+  return gulp.src('static/js/transformer.js')
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('../js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('static/vendor/bootstrap'))
+
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('static/vendor/jquery'))
+
+  gulp.src(['node_modules/jquery.easing/*.js'])
+    .pipe(gulp.dest('static/vendor/jquery-easing'))
+
+  gulp.src([
+      'node_modules/font-awesome/**',
+      '!node_modules/font-awesome/**/*.map',
+      '!node_modules/font-awesome/.npmignore',
+      '!node_modules/font-awesome/*.txt',
+      '!node_modules/font-awesome/*.md',
+      '!node_modules/font-awesome/*.json'
+    ])
+    .pipe(gulp.dest('static/vendor/font-awesome'))
+
+  gulp.src([
+      'node_modules/devicons/**/*',
+      '!node_modules/devicons/*.json',
+      '!node_modules/devicons/*.md',
+      '!node_modules/devicons/!PNG',
+      '!node_modules/devicons/!PNG/**/*',
+      '!node_modules/devicons/!SVG',
+      '!node_modules/devicons/!SVG/**/*'
+    ])
+    .pipe(gulp.dest('static/vendor/devicons'))
+
+  gulp.src(['node_modules/simple-line-icons/**/*', '!node_modules/simple-line-icons/*.json', '!node_modules/simple-line-icons/*.md'])
+    .pipe(gulp.dest('static/vendor/simple-line-icons'))
+})
+
+// Default task
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('static/css/*.css', ['minify-css']);
+  gulp.watch('static/js/*.js', ['minify-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('static/js/**/*.js', browserSync.reload);
+});
